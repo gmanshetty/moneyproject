e63b2b3b86ea0bd4790e68ff09a67306e26bd5c9
Gmanshetty <gmanshetty@ymail.com>
2011-01-28 00:39:03 -0500

    Gman Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moneyproject::Application.config.secret_token = '435075532abca55b20c672f9a77dcb986a99ef5e228fd911bd44051861e952a627e55504665b4a93ead003fa01d676e76a0509f1d7583f66737aa369be42a8d1'
